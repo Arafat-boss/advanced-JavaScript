0f1481a39d7717fe76f63db694b946ffb1020b1a
JahidHasan1212 <121640674+JahidHasan1212@users.noreply.github.com>
2023-03-31 00:22:16 +0600

    map-filtter-find advance solution

---
+++ object-property.js
@@ -0,0 +1,24 @@
+const student = [
+    {id: 21, name: 'KaliBaba'},
+    {id: 241, name: 'halim'},
+    {id: 421, name: 'kamrul'},
+    {id: 261, name: 'jayed khan'},
+    {id: 66621, name: 'Sakinb'},
+];
+
+const name = student.map( s => s.name);
+const id = student.map( i => i.id);
+const manyPerson = student.filter(X => X.id>40);
+const onPerson = student.find( X => X.id>241);
+
+
+// const output = [];
+
+// for (let i = 0; i < student.length; i++) {
+//     const element = student [i];
+//     const result = element.name;
+//     output.push(result);
+// }
+
+
+console.log(onPerson); 
