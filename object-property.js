bb0dc3a07ea63a32803c4d6d15ceef9dfa30c12c
JahidHasan1212 <121640674+JahidHasan1212@users.noreply.github.com>
2023-03-31 01:09:41 +0600

    closer function use kore sohojei kaj kora jay

--- object-property.js
+++ object-property.js
@@ -5,20 +5,21 @@ const student = [
     {id: 261, name: 'jayed khan'},
     {id: 66621, name: 'Sakinb'},
 ];
-
+/*----------------update way---------- --*/
 const name = student.map( s => s.name);
 const id = student.map( i => i.id);
 const manyPerson = student.filter(X => X.id>40);
 const onPerson = student.find( X => X.id>241);
 
+/*--------------------------old Way----------*/
 
-// const output = [];
+const output = [];
 
-// for (let i = 0; i < student.length; i++) {
-//     const element = student [i];
-//     const result = element.name;
-//     output.push(result);
-// }
+for (let i = 0; i < student.length; i++) {
+    const element = student [i];
+    const result = element.name;
+    output.push(result);
+}
 
 
 console.log(onPerson); 
---
+++ scope.js
@@ -0,0 +1,6 @@
+function sum (first, second){
+    let result = first + second;
+    return result;
+}
+const add = sum(23,7);
+console.log(add);
