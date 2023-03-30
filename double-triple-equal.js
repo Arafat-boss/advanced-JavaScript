dfe6beaf2281bcb75a5b4a3f0930dd85a1ae48a0
JahidHasan1212 <121640674+JahidHasan1212@users.noreply.github.com>
2023-03-30 22:30:00 +0600

    true vs false

---
+++ double-triple-equal.js
@@ -0,0 +1,8 @@
+const arafat = 1;
+const sakil  = '1';
+if (arafat === sakil) {
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
---
+++ null-undifined.js
@@ -0,0 +1,16 @@
+//let pakhi;
+
+
+
+// function add(num1, num2){
+//     console.log(num1 + num2);
+//     return
+// }
+// const result = add(25,5);
+// console.log(result);
+
+
+// const student = {id:'23444', name:"kkk", phone:'017*******'}
+// console.log(student.gf);
+
+
